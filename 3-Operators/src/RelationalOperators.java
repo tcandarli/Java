b79ec683141747f92031d93ab3354f6d94ea860e
tcandarli <tcandarli@gmail.com>
2018-11-12 21:36:34 +0100

    Relational operators

---
+++ 3-Operators/src/RelationalOperators.java
@@ -0,0 +1,16 @@
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		
+		int i1=5;
+		int i2=10;
+		
+		System.out.println(i1==i2);  // false
+		System.out.println(i1!=i2);  // true
+		System.out.println(i1>=i2);  // false
+		System.out.println(i1<=i2);  // true
+
+	}
+
+}
---
+++ 3-Operators/src/Task_1.java
@@ -0,0 +1,16 @@
+
+public class Task_1 {
+
+	public static void main(String[] args) {
+		
+		// Task
+		double d1 = 20.0;
+		double d2 = 80.0;
+		double res1 = (d1 + d2) * 25;
+		double remainder = res1 % 40;
+		System.out.println(remainder <= 20);
+				
+
+	}
+
+}

8225d194afdc6c76577d097c7f3a43c411cb83da
tcandarli <tcandarli@gmail.com>
2018-11-19 21:20:13 +0100

    abstraction

--- 20-Abstraction/src/Main.java
+++ 20-Abstraction/src/Main.java
@@ -4,9 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 
 		Dog dog = new Dog("Yorkie");
-
 		dog.breathe();
 		dog.eat();
+
+		Parrot p1 = new Parrot("Australia Ringneck");
+		p1.breathe();
+		p1.eat();
+		p1.fly();
 
 	}
 

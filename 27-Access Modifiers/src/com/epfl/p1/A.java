7413f168533431e2f9952cd12891e8d336acb368
tcandarli <tcandarli@gmail.com>
2018-11-21 19:29:02 +0100

    Access modifiers

--- 27-Access Modifiers/src/com/epfl/p1/A.java
+++ 27-Access Modifiers/src/com/epfl/p1/A.java
@@ -1,10 +1,10 @@
 package com.epfl.p1;
 
 public class A {
-	
+
 	protected static void display() {
 		System.out.println("Java access modifiers");
-		System.out.println("Java access modifiers");
+
 	}
 
 }
--- 27-Access Modifiers/src/com/epfl/p2/B.java
+++ 27-Access Modifiers/src/com/epfl/p2/B.java
@@ -5,10 +5,9 @@ import com.epfl.p1.A;
 public class B extends A {
 
 	public static void main(String[] args) {
-		
+
 		A object = new A();
-		A.display();  
-		A.display();  
+		A.display();
 	}
 
 }

0e0b6caeb74467b81af2c318746cf902b8bfef0a
tcandarli <tcandarli@gmail.com>
2018-12-30 21:17:17 +0100

    examples

---
+++ FindEvenNumbers/src/FindEvenNumbers.java
@@ -0,0 +1,21 @@
+/*
+ * Find and print 5 even numbers between 10 and 100
+ * Return true if an even number, otherwise return false.
+ * 
+ */
+
+
+public class FindEvenNumbers {
+
+	public static void main(String[] args) {
+		
+		int startNumber = 10;
+		int finishNumber = 100;
+		
+		while(startNumber <= finishNumber) {
+			(if !isEvenNumber(startNumber))
+		}
+		
+	}
+
+}

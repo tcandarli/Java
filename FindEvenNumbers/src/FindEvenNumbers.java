c9e7710f9107086e6d1d9d878c9714552ed0c3a9
tcandarli <tcandarli@gmail.com>
2018-12-31 13:15:53 +0100

    java codes update

--- FindEvenNumbers/src/FindEvenNumbers.java
+++ FindEvenNumbers/src/FindEvenNumbers.java
@@ -1,21 +1,44 @@
 /*
  * Find and print 5 even numbers between 10 and 100
+ * Create a method called isEvenNumber that takes a parameter of type int
+ * Its purpose is to determine if the argument passed to the method is an even number or not.
  * Return true if an even number, otherwise return false.
  * 
  */
 
-
 public class FindEvenNumbers {
 
 	public static void main(String[] args) {
-		
+
 		int startNumber = 10;
 		int finishNumber = 100;
-		
-		while(startNumber <= finishNumber) {
-			(if !isEvenNumber(startNumber))
+		int evenNumbersFound = 0;
+
+		while (startNumber <= finishNumber) {
+			if (!isEvenNumber(startNumber)) {
+				startNumber++;
+				continue;
+			}
+
+			System.out.println("Even number: " + startNumber);
+			startNumber++;
+			evenNumbersFound++;
+
+			if (evenNumbersFound >= 5) {
+				break;
+			}
 		}
 		
+		System.out.println("Total even numbers found = " + evenNumbersFound);
+	}
+
+	public static boolean isEvenNumber(int number) {
+
+		if (number % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
